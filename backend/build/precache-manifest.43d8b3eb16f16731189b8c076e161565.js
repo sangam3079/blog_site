6aa6d5b03837c614478db9a7c757dfae5d471c3e
sangam79 <sangam.poudel1997@gmail.com>
2020-07-14 12:48:10 +0545

    first commit

---
+++ backend/build/precache-manifest.43d8b3eb16f16731189b8c076e161565.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c63cd61ba70b0afd1706df1c7482190",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ae662116cfa0acfc6f7",
+    "url": "/static/js/2.31a7390d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.31a7390d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b13e41a9ec3df668c14",
+    "url": "/static/js/main.726840f0.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const home = () => (
+    <div className='container'>
+        <div className="jumbotron">
+            <h1 className="display-4">Welcome to Blog Site !!</h1>
+            <p className="lead">we make all kinds of awesome blog</p>
+            <hr className="my-4"/>
+            <p>click the button below to check out blog</p>
+            <Link className="btn btn-primary btn-lg" to='/blog' role="button">Check Out</Link>
+        </div>
+    </div>
+    
+);
+
+export default home;
